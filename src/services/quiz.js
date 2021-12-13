5d8046672d21e48a9fb0aa3fe116bab66c12aee5
Rusty <enigma994@live.co.uk>
2021-12-13 15:33:50 +0000

    Correcting player limit check

--- src/services/quiz.js
+++ src/services/quiz.js
@@ -277,7 +277,7 @@ async function submitAnswerAsync(roomCode, playerName, answer) {
 
 function start(instance) {
 
-    if (instance.players.length > 2) {
+    if (instance.players.length < 2) {
         throw new InvalidOperationError('Not enough players yet');
     }
     instance.questionNumber = 1;
